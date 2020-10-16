7525363d218bb4a789026c6f93a1a7e8d8d90852
Vivek T A <vivekta20@gmail.com>
2020-10-16 20:59:59 +0530

    added BalanceTransfer back as a dependency

---
+++ FrontEnd/src/components/BalanceTransfer/BalanceTransfer.vue
@@ -0,0 +1,31 @@
+<template src="./template.html"></template>
+
+<script>
+import TabBar from '@/components/TabBar/TabBar.vue';
+
+export default {
+  name: 'balance-transfer',
+  components: {
+    TabBar,
+  },
+  data() {
+    return {
+      activeTab: 'TRADE-ALOGS',
+      tabs: [
+        'TRADE-ALOGS',        
+      ],
+      tabRouteMapping: {
+        'TRADE-ALOGS': '/balance',            
+      },
+    };
+  },
+  methods: {
+    onTabChange(activeTab) {
+      this.activeTab = activeTab;
+    },
+  },
+};
+</script>
+<style lang="scss" src="./style.scss" scoped>
+
+</style>
