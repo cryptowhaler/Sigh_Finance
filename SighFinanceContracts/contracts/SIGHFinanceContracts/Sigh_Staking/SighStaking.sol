16572b46712ef764242159911ea755bc446abd5a
“pretentiouspunjabiguy@gmail.com” <pretentiouspunjabiguy@gmail.com>
2020-11-02 13:07:54 +0530

    back to work

--- SighFinanceContracts/contracts/SIGHFinanceContracts/Sigh_Staking/SighStaking.sol
+++ SighFinanceContracts/contracts/SIGHFinanceContracts/Sigh_Staking/SighStaking.sol
@@ -3,15 +3,13 @@ pragma solidity ^0.5.0;
 /**
  * @title Sigh Staking Contract
  * @notice Distributes rewards (Fee collected from Lending Protocol ) to SIGH Stakers
- * @dev 
  * @author SIGH Finance
  */
 
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "../Math/Exponential.sol";
 
 import "../../openzeppelin-upgradeability/VersionedInitializable.sol";
 import "../interfaces/IKyberNetworkProxyInterface.sol";
@@ -23,97 +21,46 @@ contract SighStaking is VersionedInitializable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    mapping (address => uint256) private stakingBalances;
+    IERC20 private sigh_Instrument;
+
+     struct instrumentState {
+        string name;
+        uint256 index;
+        uint totalAmountRewarded;
+        uint lastUpdatedBlock;
+
+    }
+
+    mapping (address => instrumentState) private instrumentStates;
+    address[] private instrumentsRewarded;
+    address private instrumentBeingRewarded;                // Instrument being distributed among the stakers
     uint private totalStakedSigh;
 
-    event SighStaked(address staker, uint256 amount, uint256 totalStakedAmountByStaker, uint256 totalStakedSigh );
+    uint private rewardDistributionSpeed;
+
+    struct stakingBalance {
+        bool alreadyAStaker;
+        uint256 index;
+        mapping (address => uint)  instrumentAccumulated;
+        uint stakedBalance;
+    }
+
+    mapping (address => stakingBalance ) private stakingBalances;
+    uint private numberOfStakers;
 
     uint256 public constant IMPLEMENTATION_REVISION = 0x1;
+
+
+    //only SIGH Distribution Manager can use functions affected by this modifier
+    modifier onlySighFinanceConfigurator {
+        require(addressesProvider.getSIGHFinanceConfigurator() == msg.sender, "The caller must be the SIGH Finanace Configurator Contract");
+        _;
+    }
 
 
 // ########################################################
 // ##############  INITIALIZING THE STATE   ###############
 // ########################################################
-
-    /// @notice Called by the proxy when setting this contract as implementation
-    function initialize(  address addressesProvider_ ) public initializer {
-        addressesProvider = addressesProvider_;
-    }
-
-
-
-// ################################################################
-// ##############  STAKE SIGH   ###############
-// ################################################################
-
-
-    function stakeSigh(uint amount) external returns (bool) {
-
-        uint prevBalance = sigh_Instrument.balanceOf(address(this));
-        require(sigh_Instrument.transferFrom( msg.sender, address(this), amount ),"SIGH could not be transferred to the Staking Contract" );
-        uint newBalance = sigh_Instrument.balanceOf(address(this));
-
-        uint diff = sub( newBalance,prevBalance,"New Sigh balance is less than the previous balance." );
-        require(diff == amount,"Amount to be staked and the amount transferred do not match");
-
-        uint prevStakedAmount = stakingBalances[msg.sender];
-        stakingBalances[msg.sender] = add(prevStakedAmount,amount,"New Staking balance overflow");
-
-        uint prevStakedSigh = totalStakedSigh;
-        totalStakedSigh = add(prevStakedSigh, amount,"Total Staked Sigh overflow");
-
-        SighStaked( msg.sender, amount, stakingBalances[msg.sender], totalStakedSigh  );
-        return true;
-    }
-
-    function unstakeSigh(uint amount) external returns (bool) {
-        
-    }
-
-
-
-    /// @notice Distributes a list of _tokens balances in this contract, depending on the distribution
-    /// @param _tokens list of ERC20 tokens to distribute
-    function distribute(IERC20[] memory _tokens) public {
-
-        for (uint256 i = 0; i < _tokens.length; i++) {
-            address _tokenAddress = address(_tokens[i]);
-            uint256 _balanceToDistribute = (_tokenAddress != EthAddressLib.ethAddress()) ? _tokens[i].balanceOf(address(this)) : address(this).balance;
-            if (_balanceToDistribute <= 0) {
-                continue;
-            }
-
-            Distribution memory _distribution = distribution;
-            for (uint256 j = 0; j < _distribution.receivers.length; j++) {
-
-                uint256 _amount = _balanceToDistribute.mul(_distribution.percentages[j]).div(DISTRIBUTION_BASE);
-
-                if (_distribution.receivers[j] != address(0)) {
-                    if (_tokenAddress != EthAddressLib.ethAddress()) {
-                        _tokens[i].safeTransfer(_distribution.receivers[j], _amount);
-                    } 
-                    else {
-                        (bool _success,) = _distribution.receivers[j].call.value(_amount)("");
-                        require(_success, "Reverted ETH transfer");
-                    }
-                    emit Distributed(_distribution.receivers[j], _distribution.percentages[j], _amount);
-                } 
-                else {
-                    uint256 _amountToBurn = _amount;
-                    if (_tokenAddress != tokenToBurn) {             // If the token to burn is already tokenToBurn, we don't trade, burning directly
-                        _amountToBurn = internalTrade(_tokenAddress, _amount);
-                    }
-                    internalBurn(_amountToBurn);
-                }
-            }
-        }
-    }
-
-
-// ###################################################
-// ##############  INTERNAL FUNCTIONS  ###############
-// ###################################################
-
 
     /// @notice Gets the revision number of the contract
     /// @return The revision numeric reference
@@ -121,50 +68,186 @@ contract SighStaking is VersionedInitializable {
         return IMPLEMENTATION_REVISION;
     }
 
-    /// @notice Internal trade function
-    /// @param _from The token to trade from
-    /// @param _amount The amount to trade
-    function internalTrade(address _from, uint256 _amount) internal returns(uint256) {
-        address _kyberFromRef = (_from == EthAddressLib.ethAddress()) ? KYBER_ETH_MOCK_ADDRESS : _from;
-        uint256 _value = (_from == EthAddressLib.ethAddress()) ? _amount    :    0;
-            
-            // IERC20(_kyberFromRef),                              // _from token (or ETH mock address)            
-            // _amount,                                            // amount of the _from token to trade            
-            // IERC20(tokenToBurn),                                // _to token (or ETH mock address)            
-            // address(this),                                      // address which will receive the _to token amount traded            
-            // MAX_UINT,                                           // max amount to receive, no limit, using the max uint            
-            // MIN_CONVERSION_RATE,                                // conversion rate, use 1 for market price
-            // 0x0000000000000000000000000000000000000000,         // Related with a referral program, not needed
-            // ""                                                  // Related with filtering of reserves by permisionless or not. Not needed
 
-        uint256 _amountReceived = kyberProxy.tradeWithHint.value(_value)( IERC20(_kyberFromRef), _amount,  IERC20(tokenToBurn),  address(this),  MAX_UINT,  MIN_CONVERSION_RATE, 0x0000000000000000000000000000000000000000, "" );
-        emit Trade(_kyberFromRef, _amount, _amountReceived);
-        return _amountReceived;
-    }
-
-    /// @notice Internal function to send _amount of tokenToBurn to the 0x0 address
-    /// @param _amount The amount to burn
-    function internalBurn(uint256 _amount) internal {
-        require(IERC20(tokenToBurn).transfer(recipientBurn, _amount), "INTERNAL_BURN. Reverted transfer to recipientBurn address");
-        emit Burn(_amount);
+    /// @notice Called by the proxy when setting this contract as implementation
+    function initialize(  address addressesProvider_ ) public initializer {
+        addressesProvider = addressesProvider_;
     }
 
 
+// ############################################
+// ##############  STAKE SIGH   ###############
+// ############################################
 
-// #######################################
-// ##############  RANDOM  ###############
-// #######################################
+    function stake_SIGH(uint amount) external returns (bool) {
+        require(amount > 0,"SIGH to be Staked cannot be zero.");
+
+        updateRewardIndexInternal();
+        if (stakingBalances[msg.sender].alreadyAStaker) {
+            updateUserIndexInternal();
+        }
+
+        uint prevBalance = sigh_Instrument.balanceOf(address(this));
+        require(sigh_Instrument.transferFrom( msg.sender, address(this), amount ),"SIGH could not be transferred to the Staking Contract" );
+        uint newBalance = sigh_Instrument.balanceOf(address(this));
+
+        uint diff = sub( newBalance, prevBalance, "New Sigh balance is less than the previous balance." );
+        require(diff == amount,"Amount to be staked and the amount transferred do not match");
+
+        uint prevStakedBalance = stakingBalances[msg.sender].stakedBalance ;                  
+        stakingBalances[msg.sender].stakedBalance = add(prevStakedBalance, amount, "New Staking balance overflow");    // "STAKED BALANCE" UPDATED [ADDITION] (STATE UPDATE)
+
+        uint prevTotalStakedBalance = totalStakedSigh ;                  
+        totalStakedSigh = add_(prevTotalStakedBalance, amount);                                                         // "TOTAL STAKED SIGH" [ADDITION] (STATE UPDATE)
 
 
+        if ( !stakingBalances[msg.sender].alreadyAStaker ) {                                                            // USER STAKING FOR THE FIRST TIME
+            stakingBalances[msg.sender].alreadyAStaker = true;
+            stakingBalances[msg.sender].index = instrumentStates[instrumentBeingRewarded].index;
+            numberOfStakers = add_(numberOfStakers,uint(1));
+        }
 
-    /// @notice Returns the receivers and percentages of the contract Distribution
-    /// @return receivers array of addresses and percentages array on uints
-    function getDistribution() public view returns(address[] memory receivers, uint256[] memory percentages) {
-        receivers = distribution.receivers;
-        percentages = distribution.percentages;
+        SighStaked( msg.sender, amount, stakingBalances[msg.sender].index, stakingBalances[msg.sender].stakedBalance, stakingBalances[msg.sender].instrumentAccumulated, , block.number   );
+        return true;
     }
 
-    /// @notice In order to receive ETH transfers
-    function() external payable {}
+// ##############################################
+// ##############  UNSTAKE SIGH   ###############
+// ##############################################
+
+    function unstake_SIGH(uint amount) external returns (bool) {
+        require(amount > 0,"SIGH to be Un-Staked cannot be zero.");
+        require(stakingBalances[msg.sender].alreadyAStaker,"User doesn't have any Staked amount");
+
+        updateRewardIndexInternal();
+        updateUserIndexInternal();
+
+        if ( amount > stakingBalances[msg.sender].stakedBalance ) {     // Unstake the complete amount
+            amount = stakingBalances[msg.sender].stakedBalance;
+        }
+
+        uint prevBalance = sigh_Instrument.balanceOf(address(this));
+        require(sigh_Instrument.transfer( msg.sender, amount ),"SIGH could not be transferred to the User" );
+        uint newBalance = sigh_Instrument.balanceOf(address(this));
+
+        uint diff = sub( prevBalance, newBalance, "New Sigh balance is greater than the previous balance." );
+        require(diff == amount,"Amount to be un-staked and the amount transferred do not match");
+
+        uint prevStakedBalance = stakingBalances[msg.sender].stakedBalance;                  
+        stakingBalances[msg.sender].stakedBalance = sub_(prevStakedBalance, amount, "New Staking balance underflow");    // "STAKED BALANCE" UPDATED [SUBTRACTION] (STATE UPDATE)
+
+        if (stakingBalances[msg.sender].stakedBalance == 0) {
+            stakingBalances[msg.sender].alreadyAStaker = false;
+            transferAccumulatedRewardsInternal(msg.sender);
+            stakingBalances[msg.sender].index = uint(0);
+            numberOfStakers = sub_(numberOfStakers, uint(1));
+        }
+
+        SighUnstaked( msg.sender, amount, stakingBalances[msg.sender].index, stakingBalances[msg.sender].stakedBalance, stakingBalances[msg.sender].instrumentAccumulated, , block.number   );
+        return true;
+    }
+
+// #######################################################################################################
+// ##############  INTERNAL FUNCTIONS TO UPDATE INSTRUMENT DISTRIBUTION AND USER INDEXES   ###############
+// #######################################################################################################
+
+    function updateRewardIndexInternal() internal {
+
+        instrumentState storage currentRewardInstrument = instrumentStates[instrumentBeingRewarded];
+
+        uint blockNumber = block.number;
+        uint deltaBlocks = sub_(blockNumber, uint( currentRewardInstrument.lastUpdatedBlock ), 'updateRewardIndex : Block Subtraction Underflow');    // Delta Blocks 
+        
+        if (deltaBlocks > 0 && rewardDistributionSpeed > 0 && totalStakedSigh > 0) {    
+            uint reward_Accrued = mul_(deltaBlocks, rewardDistributionSpeed);                                                // Total reward accured over delta blocks
+            Double memory ratio = fraction(reward_Accrued, totalStakedSigh);                                                 // Reward accured per Staked SIGH token
+            Double memory newIndex = add_(Double({mantissa: currentRewardInstrument.index}), ratio);                         // Updated Index
+
+            currentRewardInstrument.index = newIndex.mantissa;                                                    // STATE UPDATE - INSTRUMENT BEING DISTRIBUTED "INDEX" UPDATED
+            currentRewardInstrument.lastUpdatedBlock = blockNumber;                                               // STATE UPDATE - INSTRUMENT BEING DISTRIBUTED "BLOCK NUMBER" UPDATED
+            uint prevTotalAmountRewarded = currentRewardInstrument.totalAmountRewarded;
+            currentRewardInstrument.totalAmountRewarded = add_(prevTotalAmountRewarded, reward_Accrued);          // STATE UPDATE - INSTRUMENT BEING DISTRIBUTED "TOTAL AMOUNT REWARDED" UPDATED
+        } 
+        else if ( deltaBlocks > 0 ) {         // When no reward is accured. Block number updated
+            currentRewardInstrument.lastUpdatedBlock = blockNumber ;                                             // STATE UPDATE - INSTRUMENT BEING DISTRIBUTED "BLOCK NUMBER" UPDATED
+        }
+
+        emit updatedInstrumnetBeingDistributedIndex( instrumentBeingRewarded, instrumentStates[instrumentBeingRewarded].name, instrumentStates[instrumentBeingRewarded].index, instrumentStates[instrumentBeingRewarded].totalAmountRewarded, instrumentStates[instrumentBeingRewarded].lastUpdatedBlock );
+    }
+
+
+
+    function updateUserIndexInternal(address staker) internal {
+
+        uint instrumentIndex = instrumentStates[instrumentBeingRewarded].index;
+        uint stakerIndex = stakingBalances[staker].index;
+        stakingBalances[staker].index = instrumentIndex;                                                             // STATE UPDATE - STAKER's INDEX UPDATED  
+
+        uint deltaIndexes = sub_(instrumentIndex, stakerIndex);
+        uint newInstrumentAccumulated = mul_(deltaIndexes, stakingBalances[staker].stakedBalance ); 
+
+        uint prevInstrumentAccumulated   = stakingBalances[staker].instrumentAccumulated[instrumentBeingRewarded];
+        stakingBalances[staker].instrumentAccumulated[instrumentBeingRewarded] = add_(prevInstrumentAccumulated, newInstrumentAccumulated);   // STATE UPDATE - STAKER's "INSTRUMENT ACCUMULATED AMOUNT" UPDATED  
+
+        emit updateStakerIndexUpdated( staker, instrumentIndex, stakerIndex, deltaIndexes, newInstrumentAccumulated, stakingBalances[staker].instrumentAccumulated[instrumentBeingRewarded], block.number );    
+    }
+
+// ################################################################################
+// ##############  FUNCTION RELATED TO REWARD INSTRUMENT TRANSFER   ###############
+// ################################################################################
+
+    function claimAccumulatedInstrument(address instrumentToBeClaimed) external {
+        transferAccumulatedRewardsInternal(msg.sender, instrumentToBeClaimed);
+    }
+
+
+    function transferAccumulatedRewardsInternal(address staker, address instrumentToBeClaimed) internal {
+        IERC20 instrumentAccumulated = IERC20(instrumentToBeClaimed);       
+        uint amountToBeTransferred =  stakingBalances[msg.sender].instrumentAccumulated[instrumentToBeClaimed];   
+        require(instrumentAccumulated.transfer(staker, stakingBalances[msg.sender].instrumentAccumulated[instrumentToBeClaimed]),"Instrument Accumulated failed to be transferred to the User");
+        stakingBalances[msg.sender].instrumentAccumulated[instrumentToBeClaimed] = uint(0);
+        emit instrumentTransferred( staker, instrumentToBeClaimed, instrumentStates[instrumentToBeClaimed].name, amountToBeTransferred, block.number );
+    }
+
+
+// ######################################################### 
+// ##############  CONFIGURATION FUNCTIONS   ###############
+// ######################################################### 
+
+function setDistributionSpeed(uint newSpeed) onlySighFinanceConfigurator returns (bool) {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
