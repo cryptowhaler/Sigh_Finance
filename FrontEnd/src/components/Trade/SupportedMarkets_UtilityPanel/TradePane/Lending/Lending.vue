20eda42fa16177613d4f921fcdce1ada7e724192
“pretentiouspunjabiguy@gmail.com” <pretentiouspunjabiguy@gmail.com>
2020-11-11 20:00:13 +0530

    in progress

--- FrontEnd/src/components/Trade/SupportedMarkets_UtilityPanel/TradePane/Lending/Lending.vue
+++ FrontEnd/src/components/Trade/SupportedMarkets_UtilityPanel/TradePane/Lending/Lending.vue
@@ -2,34 +2,43 @@
 
 <script>
 import TabBar from '@/components/TabBar/TabBar.vue';
-import Supply from './Supply/Supply.vue';
-import BorrowOrder from './BorrowOrder/BorrowOrder.vue';
-import RedeemComponent from './RedeemOrder/RedeemOrder.vue';
-// import SwapTokens0xAPI from './SwapTokens0xAPI/SwapTokens0xAPI.vue';
+
+import Deposit from './Deposit/Deposit.vue';
+import Redeem from './Redeem/Redeem.vue';
+import Borrow from './Borrow/Borrow.vue';
+import Repay from './Repay/Repay.vue';
+
 import ExchangeDataEventBus from '@/eventBuses/exchangeData';
 
 export default {
-  name: 'trade-tab',
+
+  name: 'Lending',
+  
   components: {
     TabBar,
-    Supply,
-    BorrowOrder,
-    RedeemComponent,
+    Deposit,
+    Redeem,
+    Borrow,
+    Repay,
   },
+
   data() {
     return {
-      activeTab: 'Supply ',
-      selectedMarket: {},
-      tabs: ['Supply ', 'Borrow ','Redeem'],
-      preActive: 'Supply ',
+      tabs: ['Deposit','Redeem','Borrow','Repay'],
+      activeTab: 'Deposit ',
+      preActive: 'Deposit',
+      selectedInstrument: {},
     };
   },
+
   methods: {
+
     activeTabChange(activeTab) {
       this.activeTab = activeTab;
-      if (activeTab == 'Supply ') {
+      if (activeTab == 'Deposit ') {
         this.$store.commit('changeInvestTab');
-      } else {
+      } 
+      else {
         this.$store.commit('changeHedgeTab');
       }
       if (this.preActive !== activeTab) {
@@ -37,20 +46,13 @@ export default {
         this.preActive = activeTab;
       }
     },
-    // Changing Selected Pair from dropdown
-    marketChange() {
-      // console.log(this.selectedMarket);
-      this.$store.commit('addLoaderTask', 3, false);
-      this.$store.commit('selectedMarketId', this.selectedMarket.id );  //Name change (eq - ETHVUSD/DEC20)
-      this.$store.commit('selectedMarketSymbol', this.selectedMarket.symbol );   //Selected market in TradeTab - ID Change
-      this.$store.commit('selectedMarketUnderlyingSymbol', this.selectedMarket.underlyingSymbol );
-      this.$store.commit('selectedMarketUnderlyingPriceUSD', this.selectedMarket.underlyingPriceUSD );
-      this.$store.commit('selectedMarketExchangeRate', this.selectedMarket.exchangeRate );
-      this.$store.commit('selectedMarketUnderlyingAddress', this.selectedMarket.underlyingAddress );
 
-      ExchangeDataEventBus.$emit('change-selected-market', {'symbol':this.selectedMarket.symbol,  'Id':this.selectedMarket.id,  'underlyingSymbol':this.selectedMarket.underlyingSymbol,    'underlyingPriceUSD':this.selectedMarket.underlyingPriceUSD,        'exchangeRate':this.selectedMarket.exchangeRate    , 'underlyingAddress':this.selectedMarket.underlyingAddress   });    //TO CHANGE ORDER-BOOK/Supported-Money-Markets
-      // ExchangeDataEventBus.$emit('change-vega-header', {'Name':this.selectedMarket.data.name,'Summary':this.selectedMarket.data.instrument_name,}); //TO CHANGE HEADER LIVE FEED               
-      // ExchangeDataEventBus.$emit('change-symbol',this.$store.state.selectedPair);
+    // Selecting an Instrument from dropdown
+    updateSelectedInstrument() {
+      console.log(this.selectedInstrument);
+      this.$store.commit('addLoaderTask', 3, false);    
+      this.$store.commit('updateSelectedInstrument',this.selectedInstrument);
+      // ExchangeDataEventBus.$emit('change-selected-instrument', {'instrument':this.selectedInstrument });    //TO CHANGE ORDER-BOOK/Supported-Money-Markets
     },
   },
 };
