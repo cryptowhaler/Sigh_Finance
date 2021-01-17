d757d1d20a0e865005248580b31482789d1229ca
“pretentiouspunjabiguy@gmail.com” <pretentiouspunjabiguy@gmail.com>
2021-01-17 18:35:19 +0530

    refactoring

--- SighFinanceContracts/contracts/LendingProtocolContracts/interfaces/ILendingPool.sol
+++ SighFinanceContracts/contracts/LendingProtocolContracts/interfaces/ILendingPool.sol
@@ -2,16 +2,146 @@ pragma solidity ^0.5.0;
 
 interface ILendingPool {
 
+
+// #####################
+// ######  EVENTS ######
+// #####################
+
+
+
+  /**
+   * @dev Emitted on deposit()
+   * @param reserve The address of the underlying asset of the reserve
+   * @param user The address initiating the deposit
+   * @param onBehalfOf The beneficiary of the deposit, receiving the aTokens
+   * @param amount The amount deposited
+   * @param depositFee The fee paid
+   * @param _boosterId The ID of the Booster used to get discount
+   **/
+  event Deposit( address indexed reserve, address indexed user,  uint256 amount, , uint256 depositFee, uint16 _boosterId);
+
+
+    /**
+    * @dev emitted during a redeem action.
+    * @param _instrument the address of the instrument
+    * @param _user the address of the user
+    * @param _amount the amount redeemed
+    **/
+    event Withdraw(address indexed _instrument, address indexed _user, uint256 _amount);
+
+  /**
+   * @dev Emitted on borrow() and flashLoan() when debt needs to be opened
+   * @param reserve The address of the underlying asset being borrowed
+   * @param user The address of the user initiating the borrow(), receiving the funds on borrow() or just
+   * initiator of the transaction on flashLoan()
+   * @param onBehalfOf The address that will be getting the debt
+   * @param amount The amount borrowed out
+   * @param borrowRateMode The rate mode: 1 for Stable, 2 for Variable
+   * @param borrowRate The numeric rate at which the user has borrowed
+   * @param _borrowFee The fee paid   
+   * @param _boosterId The ID of the Booster used to get discount
+   **/
+  event Borrow( address indexed reserve, address user, address indexed onBehalfOf, uint256 amount, uint256 borrowRateMode, uint256 borrowRate, uint256 _borrowFee, uint16 _boosterId );
+
+  /**
+   * @dev Emitted on repay()
+   * @param reserve The address of the underlying asset of the reserve
+   * @param user The beneficiary of the repayment, getting his debt reduced
+   * @param repayer The address of the user initiating the repay(), providing the funds
+   * @param amount The amount repaid
+   **/
+  event Repay( address indexed reserve, address indexed user, address indexed repayer, uint256 amount);
+
+  /**
+   * @dev Emitted on swapBorrowRateMode()
+   * @param reserve The address of the underlying asset of the reserve
+   * @param user The address of the user swapping his rate mode
+   * @param rateMode The rate mode that the user wants to swap to
+   **/
+  event Swap(address indexed reserve, address indexed user, uint256 rateMode);
+
+  /**
+   * @dev Emitted on setUserUseReserveAsCollateral()
+   * @param reserve The address of the underlying asset of the reserve
+   * @param user The address of the user enabling the usage as collateral
+   **/
+  event ReserveUsedAsCollateralEnabled(address indexed reserve, address indexed user);
+
+  /**
+   * @dev Emitted on setUserUseReserveAsCollateral()
+   * @param reserve The address of the underlying asset of the reserve
+   * @param user The address of the user enabling the usage as collateral
+   **/
+  event ReserveUsedAsCollateralDisabled(address indexed reserve, address indexed user);
+
+  /**
+   * @dev Emitted on rebalanceStableBorrowRate()
+   * @param reserve The address of the underlying asset of the reserve
+   * @param user The address of the user for which the rebalance has been executed
+   **/
+  event RebalanceStableBorrowRate(address indexed reserve, address indexed user);
+
+  /**
+   * @dev Emitted on flashLoan()
+   * @param target The address of the flash loan receiver contract
+   * @param initiator The address initiating the flash loan
+   * @param asset The address of the asset being flash borrowed
+   * @param amount The amount flash borrowed
+   * @param premium The fee flash borrowed
+   * @param _boosterId The referral code used
+   **/
+  event FlashLoan( address indexed target, address indexed initiator, address indexed asset, uint256 amount, uint256 premium, uint16 _boosterId );
+
+  /**
+   * @dev Emitted when a borrower is liquidated. This event is emitted by the LendingPool via
+   * LendingPoolCollateral manager using a DELEGATECALL
+   * This allows to have the events in the generated ABI for LendingPool.
+   * @param collateralAsset The address of the underlying asset used as collateral, to receive as result of the liquidation
+   * @param debtAsset The address of the underlying borrowed asset to be repaid with the liquidation
+   * @param user The address of the borrower getting liquidated
+   * @param debtToCover The debt amount of borrowed `asset` the liquidator wants to cover
+   * @param liquidatedCollateralAmount The amount of collateral received by the liiquidator
+   * @param liquidator The address of the liquidator
+   * @param receiveAToken `true` if the liquidators wants to receive the collateral aTokens, `false` if he wants
+   * to receive the underlying collateral asset directly
+   **/
+  event LiquidationCall(  address indexed collateralAsset,  address indexed debtAsset,  address indexed user,  uint256 debtToCover,  uint256 liquidatedCollateralAmount,  address liquidator,  bool receiveAToken );
+
+  /**
+   * @dev Emitted when the state of a reserve is updated. NOTE: This event is actually declared
+   * in the ReserveLogic library and emitted in the updateInterestRates() function. Since the function is internal,
+   * the event will actually be fired by the LendingPool contract. The event is therefore replicated here so it
+   * gets added to the LendingPool ABI
+   * @param reserve The address of the underlying asset of the reserve
+   * @param liquidityRate The new liquidity rate
+   * @param stableBorrowRate The new stable borrow rate
+   * @param variableBorrowRate The new variable borrow rate
+   * @param liquidityIndex The new liquidity index
+   * @param variableBorrowIndex The new variable borrow index
+   **/
+  event ReserveDataUpdated( address indexed reserve, uint256 liquidityRate, uint256 stableBorrowRate, uint256 variableBorrowRate, uint256 liquidityIndex, uint256 variableBorrowIndex);
+
+
+  /**
+   * @dev Emitted when the pause is triggered.
+   */
+  event Paused();
+
+  /**
+   * @dev Emitted when the pause is lifted.
+   */
+  event Unpaused();
+
 // ###########################################################
 // ######  EXTERNAL FUNCTIONS THAT USERS INTERACT WITH  ######
 // ###########################################################
     function refreshConfig() external;  // Called by LendingPool Configurator - to refresh addresses
 
-    function deposit(address _instrument, uint256 _amount, uint16 _referralCode) external payable ;
+    function deposit(address _instrument, uint256 _amount, uint16 __boosterId) external payable ;
 
     function redeemUnderlying( address _instrument,address payable _user,uint256 _amount, uint256 _ITokenBalanceAfterRedeem) external; 
 
-    function borrow(address _instrument,uint256 _amount,uint256 _interestRateMode,uint16 _referralCode) external;
+    function borrow(address _instrument,uint256 _amount,uint256 _interestRateMode,uint16 __boosterId) external;
 
     function repay(address _instrument, uint256 _amount, address payable _onBehalfOf) external payable;
 
